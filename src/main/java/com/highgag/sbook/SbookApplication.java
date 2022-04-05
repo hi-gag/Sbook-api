530f24300fe9e3689294f2a067ef858ca23ef2b2
430lyj <430lyj@gmail.com>
2022-04-06 02:52:58 +0900

    init

---
+++ src/main/java/com/highgag/sbook/SbookApplication.java
@@ -0,0 +1,13 @@
+package com.highgag.sbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/highgag/sbook/SbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highgag.sbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
